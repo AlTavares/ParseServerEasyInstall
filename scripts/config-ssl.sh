f3a25f03467bbf569d4c5b1ccbf90d459e4acd1e
AlTavares <alexandre@live.in>
2016-05-13 18:40:46 -0300

    Generating a strong Diffie-Hellman Group

--- scripts/config-ssl.sh
+++ scripts/config-ssl.sh
@@ -5,9 +5,9 @@ sudo git clone https://github.com/certbot/certbot.git /opt/letsencrypt
 
 /opt/letsencrypt/letsencrypt-auto certonly -a webroot --webroot-path=/usr/share/nginx/html -d altavares.tk -d www.altavares.tk
 
-sudo service nginx reload
-
 export EDITOR=nano
 crontab -l | { cat; echo "30 2 * * 1 /opt/letsencrypt/letsencrypt-auto renew >> /var/log/le-renew.log"; } | crontab -
 crontab -l | { cat; echo "35 2 * * 1 /etc/init.d/nginx reload"; } | crontab -
 
+sudo openssl dhparam -out /etc/ssl/certs/dhparam.pem 2048
+sudo service nginx reload
